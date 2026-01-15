70657ccd5a8ea0b77d6a0cd6ee76e77e41d41591
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-14 22:31:03 -0300

    Excluir ChatBox.js

--- app/ChatBox.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ChatBox() {
-  return (
-    <div style={{ background: "red", color: "#fff", padding: 40 }}>
-      CHATBOX CORRETO SENDO USADO
-    </div>
-  );
-}
