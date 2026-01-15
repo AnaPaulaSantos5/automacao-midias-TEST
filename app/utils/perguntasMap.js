9705a4d00a32f3edee30508b99a8fae844f85853
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-15 16:16:18 -0300

    Add perguntasMap with questions for user input

---
+++ app/utils/perguntasMap.js
@@ -0,0 +1,18 @@
+const perguntasMap = {
+  produto: "Qual produto deseja divulgar?",
+  subtipo: "Qual tipo deseja divulgar?",
+  canal: "Esse flyer será para qual canal?",
+  formato: "Qual formato você deseja?",
+
+  campanha: "Deseja usar alguma campanha? Se sim, qual?",
+  tabela: "Deseja incluir tabela de valores?",
+  colunasTabela: "Quais colunas deseja adicionar na tabela?",
+  valoresTabela: "Quais são os valores da tabela?",
+  textoLegal: "Deseja adicionar algum texto legal ou observação?",
+
+  valorCarta: "Qual o valor da carta?",
+  prazo: "Qual o prazo do plano?",
+  parcelaInicial: "Qual o valor da parcela inicial?"
+};
+
+export default perguntasMap;
