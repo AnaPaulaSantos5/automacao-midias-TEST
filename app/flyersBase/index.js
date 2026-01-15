ae6e0cd42c835fbd7a051095dfd09596ec1fe1f2
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-15 15:36:46 -0300

    Create flyersBase module to aggregate configurations

---
+++ app/flyersBase/index.js
@@ -0,0 +1,11 @@
+import confiFinancas from "./confi-financas";
+import confiSeguros from "./confi-seguros";
+import confiBeneficios from "./confi-beneficios";
+
+const flyersBase = {
+  financas: confiFinancas,
+  seguros: confiSeguros,
+  beneficios: confiBeneficios
+};
+
+export default flyersBase;
