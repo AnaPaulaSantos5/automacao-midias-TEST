c3f3f84df3db25d150a2f2117f45c33529434e26
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-15 15:33:53 -0300

    Create index.js for Confi Benefícios configuration

---
+++ app/flyersBase/confi-beneficios/index.js
@@ -0,0 +1,16 @@
+import saude from "./saude";
+import odonto from "./odonto";
+import pet from "./pet";
+
+const confiBeneficios = {
+  area: "beneficios",
+  marca: "Confi Benefícios",
+
+  produtos: {
+    saude,
+    odonto,
+    pet
+  }
+};
+
+export default confiBeneficios;
