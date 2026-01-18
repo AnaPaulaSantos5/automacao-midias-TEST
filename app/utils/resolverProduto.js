035e4d1281c96e7f46a26db88cacaf06ba28a607
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-17 21:45:10 -0300

    Criar resolverProduto.js

---
+++ app/utils/resolverProduto.js
@@ -0,0 +1,15 @@
+import { produtos } from "@/data/produtos";
+import { normalizarTexto } from "./normalizacao";
+
+export function resolverProduto(texto) {
+  const t = normalizarTexto(texto);
+
+  for (const key of Object.keys(produtos)) {
+    const nome = normalizarTexto(produtos[key].nomeExibicao);
+    if (t.includes(nome.split(" ")[0])) {
+      return key;
+    }
+  }
+
+  return null;
+}
