300205dce6d26842d571861934aa723e8e36fa26
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-18 12:58:11 -0300

    Atualizar o ChatBox.js

--- app/components/ChatBox.js
+++ app/components/ChatBox.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useRef } from 'react';
-import { chatEngine } from '@/app/utils/chatEngine';
+import { chatEngine } from './app/utils/chatEngine';
 
 export default function ChatBox() {
   const [messages, setMessages] = useState([]);
